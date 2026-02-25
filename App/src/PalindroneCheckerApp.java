fa3ce87d691e1c2e6d8f1ca8033ca2fd71bee2d1
Charan-3132 <np3458@srmist.edu.in>
2026-02-25 09:01:15 +0530

    added welcome page

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
 public class PalindroneCheckerApp {
+    public static void main(String [] args){
+        System.out.println("Welcome To Palindrone Checker App Management System ");
+    }
 }
